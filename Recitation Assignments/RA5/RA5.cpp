b98061db5399b8feb4ff00895bb1afa6965df1d7
Zaid Abu Jumaiza <zaidxjumaiza@gmail.com>
2022-03-01 20:01:23 -0500

    ra5 done

--- Recitation Assignments/RA5/RA5.cpp
+++ Recitation Assignments/RA5/RA5.cpp
@@ -1,25 +1,50 @@
 #include <iostream>
-#include <cctype>
-#include <string>
-#include "lex.h"
+#include <map>
+#include <fstream>
 #include <vector>
-#include <algorithm>
+#include "lex.h"
+
 using namespace std;
 
 LexItem id_or_kw(const string &lexeme, int linenum)
 {
-    return LexItem(IDENT, "", 5);
+
+    // converts string to token
+    map<string, Token> StringToToken =
+        {{"IDENT", IDENT}, {"ICONST", ICONST}, {"RCONST", RCONST}, {"SCONST", SCONST}, {"PLUS", PLUS}, {"MINUS", MINUS}, {"MULT", MULT}, {"PROGRAM", PROGRAM}, {"WRITELN", WRITELN}, {"INTEGER", INTEGER}, {"BEGIN", BEGIN}, {"END", END}, {"IF", IF}, {"REAL", REAL}, {"STRING", STRING}, {"VAR", VAR}, {"ELSE", ELSE}, {"FOR", FOR}, {"THEN", THEN}, {"DO", DO}, {"TO", TO}, {"DOWNTO", DOWNTO}, {"DIV", DIV}, {":=", ASSOP}, {"LPAREN", LPAREN}, {"RPAREN", RPAREN}, {"COMMA", COMMA}, {"EQUAL", EQUAL}, {"GTHAN", GTHAN}, {"LTHAN", LTHAN}, {"SEMICOL", SEMICOL}, {"COLON", COLON}, {"ERR", ERR}, {"DONE", DONE}};
+
+    // base
+    Token currentToken = IDENT;
+
+    // iterates through map boolean value
+    map<string, Token>::iterator MapIterator = StringToToken.find(lexeme);
+
+    // determines if it is a keyword or identifier
+    if (StringToToken.end() != MapIterator)
+    {
+        currentToken = MapIterator->second;
+    }
+
+    // return lexitem with new token
+    return LexItem(currentToken, lexeme, linenum);
 }
 
 ostream &operator<<(ostream &out, const LexItem &tok)
 {
+
+    // convert token to string
+    map<Token, string> TokenToString = {{IDENT, "IDENT"}, {ICONST, "ICONST"}, {RCONST, "RCONST"}, {SCONST, "SCONST"}, {PLUS, "PLUS"}, {MINUS, "MINUS"}, {MULT, "MULT"}, {PROGRAM, "PROGRAM"}, {WRITELN, "WRITELN"}, {INTEGER, "INTEGER"}, {BEGIN, "BEGIN"}, {END, "END"}, {IF, "IF"}, {REAL, "REAL"}, {STRING, "STRING"}, {VAR, "VAR"}, {ELSE, "ELSE"}, {FOR, "FOR"}, {THEN, "THEN"}, {DO, "DO"}, {TO, "TO"}, {DOWNTO, "DOWNTO"}, {DIV, "DIV"}, {ASSOP, "ASSOP"}, {LPAREN, "LPAREN"}, {RPAREN, "RPAREN"}, {COMMA, "COMMA"}, {EQUAL, "EQUAL"}, {GTHAN, "GTHAN"}, {LTHAN, "LTHAN"}, {SEMICOL, "SEMICOL"}, {COLON, "COLON"}, {ERR, "ERR"}, {DONE, "DONE"}};
+
+    // get token of lexitem
+    Token currentToken = tok.GetToken();
+
+    // get string from token
+    string GetTokenInString = TokenToString.find(currentToken)->second;
+
+    // print
+    out << GetTokenInString << " (" << tok.GetLexeme() << ")"
+        << " Line #: " << tok.GetLinenum() << endl;
+
+    // print out
     return out;
-}
-
-int main(int argc, char *argv[])
-{
-
-    string input = argv[1];
-
-    return 0;
 }
--- Recitation Assignments/RA5/lex.h
+++ Recitation Assignments/RA5/lex.h
@@ -3,7 +3,7 @@
  *
  * CS280
  * Fall 2021
-*/
+ */
 
 #ifndef LEX_H_
 #define LEX_H_
@@ -13,42 +13,70 @@
 #include <map>
 using namespace std;
 
+// Definition of all the possible token types
+enum Token
+{
+	// keywords
+	PROGRAM,
+	WRITELN,
+	INTEGER,
+	BEGIN,
+	END,
+	IF,
+	REAL,
+	STRING,
+	VAR,
+	ELSE,
+	FOR,
+	THEN,
+	DO,
+	TO,
+	DOWNTO,
 
-//Definition of all the possible token types
-enum Token {
-		// keywords
-	PROGRAM, WRITELN, INTEGER, BEGIN, END, IF, REAL, STRING,
-	VAR, ELSE, FOR, THEN, DO, TO, DOWNTO,
-
-		// an identifier
+	// an identifier
 	IDENT,
 
-		// an integer, real, and string constant
-	ICONST, RCONST, SCONST, 
+	// an integer, real, and string constant
+	ICONST,
+	RCONST,
+	SCONST,
 
-		// the operators, parens, semicolon
-	PLUS, MINUS, MULT, DIV, ASSOP, LPAREN, RPAREN, COMMA, 
-	EQUAL, GTHAN, LTHAN, SEMICOL, COLON, 
-		// any error returns this token
+	// the operators, parens, semicolon
+	PLUS,
+	MINUS,
+	MULT,
+	DIV,
+	ASSOP,
+	LPAREN,
+	RPAREN,
+	COMMA,
+	EQUAL,
+	GTHAN,
+	LTHAN,
+	SEMICOL,
+	COLON,
+	// any error returns this token
 	ERR,
 
-		// when completed (EOF), return this token
+	// when completed (EOF), return this token
 	DONE
 };
 
-
-//Class definition of LexItem
-class LexItem {
-	Token	token;
-	string	lexeme;
-	int	lnum;
+// Class definition of LexItem
+class LexItem
+{
+	Token token;
+	string lexeme;
+	int lnum;
 
 public:
-	LexItem() {
+	LexItem()
+	{
 		token = ERR;
 		lnum = -1;
 	}
-	LexItem(Token token, string lexeme, int line) {
+	LexItem(Token token, string lexeme, int line)
+	{
 		this->token = token;
 		this->lexeme = lexeme;
 		this->lnum = line;
@@ -57,16 +85,13 @@ public:
 	bool operator==(const Token token) const { return this->token == token; }
 	bool operator!=(const Token token) const { return this->token != token; }
 
-	Token	GetToken() const { return token; }
-	string	GetLexeme() const { return lexeme; }
-	int	GetLinenum() const { return lnum; }
+	Token GetToken() const { return token; }
+	string GetLexeme() const { return lexeme; }
+	int GetLinenum() const { return lnum; }
 };
 
-
-
-extern ostream& operator<<(ostream& out, const LexItem& tok);
-extern LexItem id_or_kw(const string& lexeme, int linenum);
-extern LexItem getNextToken(istream& in, int& linenum);
-
+extern ostream &operator<<(ostream &out, const LexItem &tok);
+extern LexItem id_or_kw(const string &lexeme, int linenum);
+extern LexItem getNextToken(istream &in, int &linenum);
 
 #endif /* LEX_H_ */
