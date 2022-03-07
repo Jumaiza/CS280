f855a4042f6a93a930e04f0ca63f93c10ed1c9b5
Jumaiza <zaidxjumaiza@gmail.com>
2022-03-07 13:20:56 -0500

    ra6 done

--- Recitation Assignments/RA6/RA6.cpp
+++ Recitation Assignments/RA6/RA6.cpp
@@ -2,25 +2,47 @@
 #include <string>
 using namespace std;
 
+int maxValue;
+
 void PrintRevNums(int sum)
 {
+
     int num;
     cin >> num;
 
+    // base case. exit if input is 0
     if (num == 0)
     {
-        cout << "The maximum value is: ";
+        return;
     }
-    else
+
+    // if input is greater than current maximum value, then make this input the new maximum value
+    if (num > maxValue)
     {
+        maxValue = num;
+    }
+
+    // add input to total
+    sum += num;
+
+    // call function again recursively
+    PrintRevNums(sum);
+
+    // print input and total in reverse order
+    cout << num << " Total: " << sum << endl;
+
+    // if we get back to the first element, print max value
+    if (sum == num)
+    {
+        cout << "The maximum value is: " << maxValue << endl;
     }
 }
 
 int main()
 {
 
+    cout << "Enter any sequence of positive integer numbers, then enter 0 to end: " << endl;
     int sum = 0;
-    cout << "Enter any sequence of positive integer numbers,then enter 0 to end : ";
     PrintRevNums(sum);
     return 0;
 }
